121f5acda978408f7dd3f05ae3037f0d4f83d910
Anthony Mace <amace93@gmail.com>
2016-05-22 21:19:57 -0700

    added text fields username and password

--- app/src/main/java/com/android/a_mace/gradecalculator/LoginActivity.java
+++ app/src/main/java/com/android/a_mace/gradecalculator/LoginActivity.java
@@ -2,6 +2,7 @@ package com.android.a_mace.gradecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void logIn(View view) {
+        System.out.println("Hello World!");
     }
 }
