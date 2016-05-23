013cc8eeee50059f2dbb7d6adf05c1b467edd323
Anthony Mace <amace93@gmail.com>
2016-05-22 21:53:22 -0700

    grabbed strings from text fields

--- app/src/main/java/com/android/a_mace/gradecalculator/LoginActivity.java
+++ app/src/main/java/com/android/a_mace/gradecalculator/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.android.a_mace.gradecalculator;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,6 +15,14 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void logIn(View view) {
-        System.out.println("Hello World!");
+        String username = getStringFromEditText(R.id.username);
+        String password = getStringFromEditText(R.id.password);
+        System.out.println(username + " " + password + " logging in...");
+    }
+
+    private String getStringFromEditText(int id) {
+        EditText et = (EditText)findViewById(id);
+        String etString = et.getText().toString();
+        return etString;
     }
 }
