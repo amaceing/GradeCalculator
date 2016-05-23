17d3e191dceccedfd2f4c7e595632eef906c9dde
Anthony Mace <amace93@gmail.com>
2016-05-22 20:21:12 -0700

    changed activity name to reflect login

--- app/src/main/java/com/android/a_mace/gradecalculator/LoginActivity.java
+++ app/src/main/java/com/android/a_mace/gradecalculator/LoginActivity.java
@@ -3,11 +3,11 @@ package com.android.a_mace.gradecalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SemesterActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_semester);
+        setContentView(R.layout.activity_login);
     }
 }
